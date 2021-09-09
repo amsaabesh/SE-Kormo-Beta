d00b2410ef3fae4161d44b008a9ba82b85dafc3e
amsaabesh <mahiraabesh@gmail.com>
2021-09-09 11:40:34 +0600

    beta V0.0

---
+++ KOrmo0.0/LoginPage.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LoginPage.aspx.cs" Inherits="KOrmo0._0.LoginPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Kormo_Login</title>
+    
+    <link href="loginStyleSheet.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <section>
+            <img src="Image/bg2.jpg" class="panel"/>
+        </section>
+        <div class="sec2">
+            <div class="container">
+                <div class="social">
+                    <asp:Image ID="Image1" runat="server" ImageUrl="~/Image/round.png" />
+                </div>
+
+                <div class="content">
+                    <h2>Login</h2>
+                    <asp:TextBox ID="TextBox1" placeholder="Enter User name" runat="server"></asp:TextBox><br />
+                    <asp:TextBox ID="TextBox2" placeholder="Password" runat="server"></asp:TextBox><br />
+                    <asp:Button ID="Login" runat="server" onclick="Button1_Click" Text="Login" /><br />
+                    <div class="abc">
+                    <asp:Button ID="Button1" runat="server" onclick="Button2_Click" Text="About Us" /><br />
+                </div>
+                </div>
+                
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ KOrmo0.0/LoginPage.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+
+namespace KOrmo0._0
+{
+    public partial class LoginPage : System.Web.UI.Page
+    {
+        public void MsgBox(String ex, Page pg, Object obj)
+        {
+            string s = "<SCRIPT language='javascript'>alert('" + ex.Replace("\r\n", "\\n").Replace("'", "") + "'); </SCRIPT>";
+            Type cstype = obj.GetType();
+            ClientScriptManager cs = pg.ClientScript;
+            cs.RegisterClientScriptBlock(cstype, s, s.ToString());
+        }
+        MySqlConnection con;
+        MySqlCommand cmd;
+        string str;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        con = new MySqlConnection("Data Source=localhost;Database=dbtest;User ID=root;Password= ");
+        con.Open();
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+        str = "insert into details values ('" + TextBox1.Text + "','" + TextBox2.Text + "')";
+        cmd = new MySqlCommand(str, con);
+            MsgBox("Login Successfull! ", this.Page, this);
+            cmd.ExecuteNonQuery();
+        }
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            MsgBox("Kormo is a website. From here, skilled workers can be hired based on their profiles. We prioritize skills. So if you take our staff from here, you can make them work as you like. If we think they are safe for you and only if you are also safe for them then a deal will be done. Our payment will be online based. This will reduce the hassle a lot. On the one hand, they do not have to look for work, on the other hand, people will easily find skilled workers.", this.Page, this);
+        }
+    }
+}
